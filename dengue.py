5c4a34880862264eb854e2094b7e7ee3531660b9
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-06-19 10:43:23 -0300

    enhancing dengue

--- dengue.py
+++ dengue.py
@@ -13,6 +13,7 @@ YYYYWWFINISH = 202401
 # TODO: Regressão linear tendo como target o numero de casos e usando umidade, temperatura etc como features
 
 CITIES = get_metropolitan_cities()
+USELESS_COLS = ["casos_est_min", "casos_est_max", "p_rt1", "p_inc100k", "Localidade_id", "id", "versao_modelo", "tweet", "Rt", "tempmin", "umidmax", "nivel_inc", "umidmin", "tempmax", "casprov_est", "casprov_est_min", "casprov_est_max", "casconf", "notif_accum_year"]
 
 def get_city_data(city_name):
     city_name = city_name.replace(" ", "_")
@@ -24,6 +25,10 @@ def get_city_data(city_name):
     if df is None:
       print(f"Could not download dengue file for city {city_name}!")
       return
+    df = df.transpose()
+    df = df.drop(USELESS_COLS, axis=1)
+    df['data_iniSE'] = df['data_iniSE'].str.slice(0, 7)
+    
     df.to_csv(f'data/{city_name}.csv')
     print(f"Downloaded dengue file for city {city_name}!")
 
@@ -38,12 +43,17 @@ def concatenate_csv_files(directory):
     return combined_df
 
 def collapse_to_monthly(df):
+  print(df.columns)
   # todo
   pass
 
 
-# for city in CITIES:
-#   get_city_data(city)
+#df = pd.read_csv("data/Alvorada.csv")
+
+#collapse_to_monthly(df)
+
+for city in CITIES:
+  get_city_data(city)
 
 # combined_csv = concatenate_csv_files('data')
 # combined_csv.to_csv('combined.csv', index=False)
