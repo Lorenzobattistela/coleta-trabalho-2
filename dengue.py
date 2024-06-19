c6d10fe6e230f41803d4f6a3189297332ce5a2aa
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-06-19 19:45:27 -0300

    finishing dengue preprocessing

--- dengue.py
+++ dengue.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 from wikipedia_scrap import get_metropolitan_cities
 
-YYYYWWSTART = 202301
+YYYYWWSTART = 201301
 YYYYWWFINISH = 202401
 
 # TODO: juntar 4 colunas de semana em 1 de mês (comprimir por mes)
@@ -15,8 +15,16 @@ YYYYWWFINISH = 202401
 CITIES = get_metropolitan_cities()
 USELESS_COLS = ["casos_est_min", "casos_est_max", "p_rt1", "p_inc100k", "Localidade_id", "id", "versao_modelo", "tweet", "Rt", "tempmin", "umidmax", "nivel_inc", "umidmin", "tempmax", "casprov_est", "casprov_est_min", "casprov_est_max", "casconf", "notif_accum_year"]
 
+def clean_city_name(city_name):
+  city_name = city_name.replace(" ", "_")
+  city_name = city_name.replace("\303\241", "a")
+  city_name = city_name.replace("\303\243", "a")
+  city_name = city_name.replace("\303\255", "i")
+  city_name = city_name.replace("\303\251", "e")
+  return city_name
+
 def get_city_data(city_name):
-    city_name = city_name.replace(" ", "_")
+    city_name = clean_city_name(city_name)
     if (os.path.exists(f"./data/{city_name}.csv")):
         print(f"City {city_name} already exists.")
         return
@@ -28,32 +36,31 @@ def get_city_data(city_name):
     df = df.transpose()
     df = df.drop(USELESS_COLS, axis=1)
     df['data_iniSE'] = df['data_iniSE'].str.slice(0, 7)
-    
+    df = collapse_to_monthly(df)
+    df['city'] = city_name
+
     df.to_csv(f'data/{city_name}.csv')
     print(f"Downloaded dengue file for city {city_name}!")
 
 
-def concatenate_csv_files(directory):
-    csv_files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith('.csv')]
-
-    # Read and concatenate all CSV files
-    df_list = [pd.read_csv(file) for file in csv_files]
-    combined_df = pd.concat(df_list, ignore_index=True)
-
-    return combined_df
-
 def collapse_to_monthly(df):
-  print(df.columns)
-  # todo
-  pass
+  return df.groupby('data_iniSE').mean().reset_index()
 
+def concatenate_csv_files(directory):
+    csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
+    df_list = []
 
-#df = pd.read_csv("data/Alvorada.csv")
+    for file in csv_files:
+        file_path = os.path.join(directory, file)
+        df = pd.read_csv(file_path)
+        df_list.append(df)
 
-#collapse_to_monthly(df)
+    combined_df = pd.concat(df_list, ignore_index=True)
+    combined_df.rename(columns={'Unnamed: 0': 'id'}, inplace=True)
+    return combined_df
 
 for city in CITIES:
   get_city_data(city)
 
-# combined_csv = concatenate_csv_files('data')
-# combined_csv.to_csv('combined.csv', index=False)
+combined_csv = concatenate_csv_files('data')
+combined_csv.to_csv('combined.csv', index=False)
