237564453b8990dcf1abc3d89b891e2f91c6e691
Gabriel Stefanello <gabrielmstefanello058@gmail.com>
2024-06-22 08:15:45 -0300

    indo pro linux

--- dengue.py
+++ dengue.py
@@ -1,9 +1,13 @@
 from numpy import NAN
 import math
-from pysus.online_data.Infodengue import search_string, download
+# from pysus.online_data.Infodengue import search_string, download
 import os
 import pandas as pd
-from wikipedia_scrap import get_metropolitan_cities
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+# from wikipedia_scrap import get_metropolitan_cities
 
 YYYYWWSTART = 201301
 YYYYWWFINISH = 202401
@@ -14,9 +18,33 @@ YYYYWWFINISH = 202401
 # TODO: Plot temporal de casos por mês por cidade
 # TODO: Regressão linear tendo como target o numero de casos e usando umidade, temperatura etc como features
 
-CITIES = get_metropolitan_cities()
+# CITIES = get_metropolitan_cities()
 USELESS_COLS = ["casos_est_min", "casos_est_max", "p_rt1", "p_inc100k", "Localidade_id", "id", "versao_modelo", "tweet", "Rt", "nivel_inc", "casprov_est", "casprov_est_min", "casprov_est_max", "casconf", "notif_accum_year", "casprov"]
 TEMP_UMID_COLS = ["tempmed", "tempmax", "tempmin", "umidmed", "umidmin", "umidmax"]
+
+
+
+def create_temporal_series(csv_path):
+    data = pd.read_csv(csv_path)
+    X = data[["temperatura","umidade","pop"]]
+    Y = data["casos"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    # Calculate metrics
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print(f'Mean Squared Error: {mse}')
+    print(f'R-squared: {r2}')
+
+    # Display the coefficients
+    coefficients = pd.DataFrame(model.coef_, X.columns,columns=['Coefficient'])
+    print(coefficients)
+
 
 def clean_city_name(city_name):
   city_name = city_name.replace(" ", "_")
@@ -123,10 +151,16 @@ def concatenate_csv_files(directory):
     combined_df.rename(columns={'Unnamed: 0': 'id', 'umidmed': 'umidade', 'tempmed': 'temperatura'}, inplace=True)
     return combined_df
 
-for city in CITIES:
-  get_city_data(city)
+# for city in CITIES:
+#   get_city_data(city)
 
-combined_csv = concatenate_csv_files('data')
-combined_csv.to_csv('combined.csv', index=False)
-combined_csv = pd.read_csv("combined.csv")
-print(combined_csv.isnull().sum())
+
+
+
+if __name__ == "__main__":
+
+    # combined_csv = concatenate_csv_files('data')
+    # combined_csv.to_csv('combined.csv', index=False)
+    # combined_csv = pd.read_csv("combined.csv")
+    # print(combined_csv.isnull().sum())
+    create_temporal_series("combined.csv")
